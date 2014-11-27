57d4529f5eb13f1f4c16857b5f6a3059cb00e9c6
Vagabond Gaming <vagabondgaming@live.com.au>
2014-11-28 09:09:48 +1100

    Cloned on vMac, Created useless iOS project.

--- RoundedBoxViewRenderer/RoundedBoxViewRenderer/App.cs
+++ RoundedBoxViewRenderer/RoundedBoxViewRenderer/App.cs
@@ -7,15 +7,7 @@ namespace RoundedBoxViewRenderer
     {
         public static Page GetMainPage()
         {	
-            return new ContentPage
-            { 
-                Content = new Label
-                {
-                    Text = "Hello, Forms!",
-                    VerticalOptions = LayoutOptions.CenterAndExpand,
-                    HorizontalOptions = LayoutOptions.CenterAndExpand,
-                },
-            };
+			return new MainPage ();
         }
     }
 }
---
+++ RoundedBoxViewRenderer/RoundedBoxViewRenderer/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace RoundedBoxViewRenderer
+{	
+	public partial class MainPage : ContentPage
+	{	
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
